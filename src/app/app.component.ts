a20dd09514d00f6ca4c84fd89bc9e8315d9c0631
ashanperera201 <ashanperera.201@gmail.com>
2022-03-28 14:06:33 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { HubConnection, HubConnectionBuilder, HttpTransportType } from '@aspnet/signalr';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'rnd-application';
+  dataSet: any;
+
+  private _hubConnection: HubConnection | undefined;
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this._hubConnection = new HubConnectionBuilder()
+      .withUrl('http://localhost:56558/order',
+        {
+          skipNegotiation: true,
+          transport: HttpTransportType.WebSockets
+        })
+      .build();
+
+    this._hubConnection
+      .start()
+      .then(() => console.log("Connection Started!"))
+      .catch(err => console.log("Error while establishing a connection :( "));
+
+    this._hubConnection.on('ReceivedOrder', (data) => {
+      this.dataSet = data;
+    })
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
+import { HubConnection } from '@microsoft/signalr';
+import * as signalR from '@microsoft/signalr';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +10,16 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  // providers: [HubConnectionFactory],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  // constructor(factory: HubConnectionFactory) {
+  //   factory.create(
+  //     { key: "order", endpointUri: "/order" },
+  //   );
+  // }
+}
